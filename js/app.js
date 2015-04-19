4ed34ed2cdedf16bd878d9dd5787290f15e684b1
Raphael Faria <raphael@codelogic.me>
2015-04-19 14:34:16 -0300

    Fixes map loading problems and adds a reload button to it

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-$(function() {
+$(document).ready(function() {
 
   var winHeight = $(window).innerHeight();
   var mapContainer = $('#map');
+  var mapiFrame = $('#map__iframe');
 
   var applyHeightToMap = function(height) {
     mapContainer.innerHeight(height);
+  };
+
+  var loadMap = function(frame) {
+    frame.src = 'https://www.google.com/maps/embed/v1/place?q=Oitava%20Igreja%20Presbiteriana%20de%20Belo%20Horizonte%20-%20Rua%20Nestor%20Soares%20de%20Melo%2C%20Palmares%2C%20Belo%20Horizonte%20-%20State%20of%20Minas%20Gerais%2C%20Brazil&key=AIzaSyCi21HehWT8SewsuY2PMV3POJCUWtgPra4';
+  };
+
+  var reloadMap = function(frame) {
+    frame.src = frame.src;
   };
 
   $(window).on('resize', function() {
@@ -15,12 +24,11 @@ $(function() {
   applyHeightToMap(winHeight);
 
   mapContainer.on('click', function() {
-    console.log($('#map__iframe'));
-    $('#map__iframe').removeClass('map--scrolloff');
+    mapiFrame.removeClass('map--scrolloff');
   });
 
-  $('#map__iframe').mouseleave(function() {
-    $('#map__iframe').addClass('map--scrolloff');
+  mapiFrame.mouseleave(function() {
+    mapiFrame.addClass('map--scrolloff');
   });
 
   $('a[href*=#]:not([href=#])').click(function() {
@@ -35,4 +43,11 @@ $(function() {
       }
     }
   });
+
+  $('.map--reload').click(function() {
+    reloadMap(mapiFrame[0]);
+  });
+
+
+  loadMap(mapiFrame[0]);
 });
